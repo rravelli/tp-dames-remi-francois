5b4d6f334863403f47d17c3eb604ac72fcfe339d
Inky19 <f.marie260@gmail.com>
2022-12-09 11:23:24 +0100

    Added Dame, renamed main file in Game

--- src/main/java/edu/centralenantes/dame/Dame.java
+++ src/main/java/edu/centralenantes/dame/Dame.java
@@ -1,17 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package edu.centralenantes.dame;
 
 /**
  *
  * @author inky19
  */
-public class Dame {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Dame extends Pion {
+    
 }
---
+++ src/main/java/edu/centralenantes/dame/Game.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package edu.centralenantes.dame;
+
+/**
+ *
+ * @author inky19
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[][] board = new int[size][size];
+        
+        
+    }
+}
